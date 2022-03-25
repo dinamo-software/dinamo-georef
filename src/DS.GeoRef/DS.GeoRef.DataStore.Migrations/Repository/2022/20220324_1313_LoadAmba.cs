27f4dfc1f88b355667915fb221e2c520b399377c
Leandro_Tonello <leandronicolas1177@gmail.com>
2022-03-25 00:54:08 -0300

    Corregido ID por Code en MunicipiosAmba, creacion de tabla de Amba

--- src/DS.GeoRef/DS.GeoRef.DataStore.Migrations/Repository/2022/20220324_1313_LoadAmba.cs
+++ src/DS.GeoRef/DS.GeoRef.DataStore.Migrations/Repository/2022/20220324_1313_LoadAmba.cs
@@ -18,8 +18,46 @@ namespace DS.GeoRef.DataStore.Migrations.Repository._2022
         public override void Up()
         {
             var sequence = new Helpers.SequenceHelper();
-            Insert.IntoTable("amba").Row(new { id = sequence.Next(), code = MunicipiosConstants.AlmiranteBrown });
-            Insert.IntoTable("amba").Row(new { id = sequence.Next(), code = MunicipiosConstants.Avellaneda });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.AlmiranteBrown });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Avellaneda });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Berazategui });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Berisso });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Brandsen });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Campana });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Canuelas });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Ensenada });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Escobar });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.EstebanEcheverria });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.ExaltacionDeLaCruz });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Ezeiza });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.FlorencioVarela });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.GeneralLasHeras });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.GeneralRodriguez });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.GeneralSanMartin });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Hurlingham });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Ituzaingo });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.JoseCPaz });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.LaMatanza });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Lanus });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.LaPlata });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.LomasDeZamora });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Lujan });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.MalvinasArgentinas });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.MarcosPaz });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Merlo });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Moreno });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Moron });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Pilar });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.PresidentePeron });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Quilmes });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.SanFernando });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.SanIsidro });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.SanMiguel });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.SanVicente });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Tigre });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.TresdeFebrero });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.VicenteLopez });
+            Insert.IntoTable("amba").Row(new { id = sequence.Next(), municipio_code = MunicipiosConstants.Zarate });
         }
 
         public override void Down()
