d1d659ce2a89168bcf32b9ce6827c16abaf09faa
Hernan Casado <hcasado@gmail.com>
2022-05-13 15:15:54 -0300

    fix migration number and rename task table to singular

--- src/DS.GeoRef/DS.GeoRef.DataStore.Migrations/Repository/2022/20220510_1615_CreateTable_Tareas.cs
+++ src/DS.GeoRef/DS.GeoRef.DataStore.Migrations/Repository/2022/20220510_1615_CreateTable_Tareas.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace DS.GeoRef.DataStore.Migrations.Repository._2022
 {
-    [Migration(202203202020, "Create Table Tareas")]
+    [Migration(20220510_1615, "Create Table Tarea")]
     public class _20220510_1615_CreateTable_Tareas : Migration
     {
         public override void Up()
         {
             //id, code, name, type, completed (boolean)
-            Create.Table("tareas")
-                .WithColumn("id").AsInt32().NotNullable().PrimaryKey("PK_Tareas")
+            Create.Table("tarea")
+                .WithColumn("id").AsInt32().NotNullable().Unique().Identity().PrimaryKey("PK_Tarea")
                 .WithColumn("code").AsString(10).NotNullable().Unique()
                 .WithColumn("name").AsString(100).NotNullable()
                 .WithColumn("type").AsString(100).Nullable()
@@ -21,7 +21,7 @@ namespace DS.GeoRef.DataStore.Migrations.Repository._2022
 
         public override void Down()
         {
-            Delete.Table("tareas");
+            Delete.Table("tarea");
         }
     }
 }
